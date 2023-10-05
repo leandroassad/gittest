04dedbfefdf69b3278c9d4874094bdbe5e2121c3
leandroassad <leandroassad@hotmail.com>
2023-10-05 14:58:43 -0300

    Incluida msg de Teste 3

--- src/main/java/com/gittest/Main.java
+++ src/main/java/com/gittest/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Teste 1");
         System.out.println("Teste 2");
+        System.out.println("Teste 3");
     }
 }
